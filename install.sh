03c64fab64eb663c96cdce4ce2be7d97ca1b0793
Nikola <nikola.91390@gmail.com>
2025-10-05 04:00:39 +0200

    Add installation script to download test script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SCRIPT_URL="https://raw.githubusercontent.com/NikoStano/push_swap_tester/refs/heads/main/test_push_swap.sh"
+NAME="test_ps.sh"
+echo "Téléchargement du script..."
+curl -fsSL "$SCRIPT_URL" -o "$NAME"
+if [ -f "$NAME" ]; then
+    chmod +x "$NAME"
+    echo "✅ Script téléchargé et rendu exécutable : ./$NAME"
+else
+    echo "❌ Erreur : le téléchargement a échoué."
+    exit 1
+fi
